e69ed2f4cafe2026aa93354bdfa9b119168bb3c6
braian-vaylet <braian.vaylet@globant.com>
2023-05-13 11:11:31 -0300

    asd

--- src/useRedirectToApp.jsx
+++ src/useRedirectToApp.jsx
@@ -7,19 +7,23 @@ const playStoreUrl =
   "https://play.google.com/store/apps/details?id=com.nera.neraagro";
 const appStoreUrl = "https://apps.apple.com/app/nera/id1667637863";
 
-const useRedirectToApp = () => {
+const handleAppScheme = (scheme) => {
+  window.location.href = `${scheme}://`;
+  setTimeout(function () {
+    window.location.href = isAndroid ? playStoreUrl : appStoreUrl;
+  }, 1000);
+}
+
+const useRedirectToApp = ({appScheme = customScheme, ask = true})  => {  
   useEffect(() => {
     const redirectToApp = async () => {
       try {
-        if (
-          isMobile &&
-          window.confirm("Quieres usar la app de Nera?") === true
-        ) {
-          window.location.href = `${customScheme}://`;
-          setTimeout(function () {
-            window.location.href = isAndroid ? playStoreUrl : appStoreUrl;
-          }, 1000);
+        if (isMobile && ask && window.confirm("Quieres usar la app de Nera?") === true) {            
+          handleAppScheme(appScheme);
         }
+        if ( isMobile && !ask) {
+          handleAppScheme(appScheme);
+        }                   
       } catch (error) {
         console.error("error", error);
       }
@@ -30,3 +34,5 @@ const useRedirectToApp = () => {
 };
 
 export default useRedirectToApp;
+
+"agtech-lib": "git+https://github.com/AgTechHub/agtech-lib.git#main",
