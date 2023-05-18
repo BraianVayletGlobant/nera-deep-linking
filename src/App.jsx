2d0e439c86f16bedf380de71bda10f857a2c2baf
braian-vaylet <braian.vaylet@globant.com>
2023-05-18 12:00:48 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     } else {
       console.log("false", false);
       // Envía un mensaje al WebView indicando que se debe compartir
-      window.postMessage("nera-share");
+      window.ReactNativeWebView.postMessage("nera-share");
     }
   };
 
